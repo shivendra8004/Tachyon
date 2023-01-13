b23dbbb85a874768b31d18f8a4561c43447a3b86
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-13 19:19:41 +0530

    contact form

--- js/main.js
+++ js/main.js
@@ -575,3 +575,23 @@ function step2() {
 
 init();
 step2();
+
+// ==============================Contact Us=================================
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
